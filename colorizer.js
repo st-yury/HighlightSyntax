3a35c80289cb20dd7c3e8d268edf65b3fbba7c6c
White rabbit <141726094+st-yury@users.noreply.github.com>
2024-03-28 20:08:08 +0300

    - Add a func to highlight chars

--- colorizer.js
+++ colorizer.js
@@ -1,28 +1,32 @@
     const csharpStateSetOfKeywords = 
-    
-    [ "async ", "await ", " in ", " as ", "abstract ", "base", "bool ", "break", "byte", "case", "readonly",
-      "catch", "char ", "checked", "class", "const", "continue", "fixed", 
-      "decimal", "default", "delegate", " do ", "double", "sizeof",
-      "enum", "explicit", "extern", "event", "false", "finally", "stackalloc",
-      "float", "goto", "implicit", "static ", "partial", " int ",
-      "int ", "interface", "internal", " is ", "lock", "long", "unit", "get", "set",
-      "namespace", "new ", "int[", "null", "object", "operator", "out", "ulong",
-      "override", "params", "private", "protected", "public", "unchecked",
-      "ref", "sbyte", "sealed", "short", "string", "virtual", " where ",
-      "struct", "switch", "this", "with", "true", "try", "void", "var ",
-      "unsafe", "ushort", "using", "typeof", "volatile", "<char", "new(", "int."
+    [ 
+        "async", "await", "in", "as", "abstract", "base", "bool", "break", "byte", 
+        "catch", "char", "checked", "class", "const", "continue", "fixed", 
+        "decimal", "default", "delegate", "do", "double", "sizeof",
+        "enum", "explicit", "extern", "event", "false", "finally", "stackalloc",
+        "float", "goto", "implicit", "static", "partial", "case", "readonly",
+        "interface", "internal", "lock", "long", "unit", "get", "set",
+        "namespace", "new", "int", "null", "object", "operator", "out", "ulong",
+        "override", "params", "private", "protected", "public", "unchecked",
+        "ref", "sbyte", "sealed", "short", "string", "virtual", "where",
+        "struct", "switch", "this", "with", "true", "try", "void", "var",
+        "unsafe", "ushort", "using", "typeof", "volatile", "is"
     ];
 
     const csharpBehaviorSetOfKeywords = 
-    
     [
-        "return ", "if", "while", "foreach", "for ", "throw", "else"
+        "return", "if", "while", "foreach", "for", "throw", "else"
     ];
 
     const specialChars = { 
         "<" : "&lt;", 
         ">" : "&gt;"
     };
+
+    const additionalKeywordChars = 
+    [
+        " ", "[", "]", "(", ")", "=", ".", "{", "}", ";", "/"
+    ];
     
     function specialCharsShielding(theCode) {
 
@@ -54,14 +58,16 @@
 
                 insideTheSingleLineComment = true;
 
-                theCode = theCode.slice(0, index) + "<oneLineComment>" + theCode.slice(index);
+                theCode = theCode.slice(0, index) + "<ignore><oneLineComment>" + theCode.slice(index);
+                index += "<ignore><oneLineComment>".length;
             }
             else 
             if (theCode[index] === "\n" && insideTheSingleLineComment) {
                 
                 insideTheSingleLineComment = false;
 
-                theCode = theCode.slice(0, index) + "</oneLineComment>" + theCode.slice(index);
+                theCode = theCode.slice(0, index) + "</oneLineComment></ignore>" + theCode.slice(index);
+                index += "</oneLineComment></ignore>".length;
             }
             else 
             if (fourCharsInSequence == "/// " 
@@ -72,22 +78,24 @@
                 
                 insideTheDocComment = true;
 
-                theCode = theCode.slice(0, index) + "<docComment>" + theCode.slice(index);
+                theCode = theCode.slice(0, index) + "<ignore><docComment>" + theCode.slice(index);
+                index += "<ignore><docComment>".length;
             }
             else
             if (theCode[index] === "\n" && insideTheDocComment) {
                 
                 insideTheDocComment = false;
 
-                theCode = theCode.slice(0, index) + "</docComment>" + theCode.slice(index);
+                theCode = theCode.slice(0, index) + "</docComment></ignore>" + theCode.slice(index);
+                index += "</docComment></ignore>".length;
             }
             else 
             if(theCode[index] === "\"" && !insideTheString && !insideTheSingleLineComment && !insideTheMultiLineComment && !insideTheDocComment) {
                 
                 insideTheString = true;
 
-                theCode = theCode.slice(0, index) + "<str>" + theCode.slice(index);
-                index += "<str>".length;
+                theCode = theCode.slice(0, index) + "<ignore><str>" + theCode.slice(index);
+                index += "<ignore><str>".length;
             }
             else
             if(theCode[index] === "\"" && theCode[index - 1] === "\\" && insideTheString && !insideTheSingleLineComment && !insideTheMultiLineComment && !insideTheDocComment) {
@@ -99,22 +107,24 @@
                 
                 insideTheString = false;
 
-                theCode = theCode.slice(0, index + 1) + "</str>" + theCode.slice(index + 1); // 1 is length of "
-                index += "</str>".length;
+                theCode = theCode.slice(0, index + 1) + "</str></ignore>" + theCode.slice(index + 1); // 1 is length of "
+                index += "</str></ignore>".length;
             }
             else
             if(twoCharsInSequence === "/*" && !insideTheString && !insideTheSingleLineComment && !insideTheDocComment && !insideTheMultiLineComment) {
                 
                 insideTheMultiLineComment = true;
 
-                theCode = theCode.slice(0, index) + "<multiLineComment>" + theCode.slice(index);
+                theCode = theCode.slice(0, index) + "<ignore><multiLineComment>" + theCode.slice(index);
+                index += "<ignore><multiLineComment>".length;
             }
             else
             if(twoCharsInSequence === "*/" && !insideTheString && !insideTheSingleLineComment && !insideTheDocComment && insideTheMultiLineComment) {
                 
                 insideTheMultiLineComment = false;
 
-                theCode = theCode.slice(0, index + 2) + "</multiLineComment>" + theCode.slice(index + 2); // 2 is length of */
+                theCode = theCode.slice(0, index + 2) + "</multiLineComment></ignore>" + theCode.slice(index + 2); // 2 is length of */
+                index += "</multiLineComment></ignore>".length;
             }
             else
             if(theCode[index] === "$" && !insideTheString && !insideTheSingleLineComment && !insideTheDocComment && !insideTheMultiLineComment) {
@@ -238,22 +248,47 @@
         return false;
     }
 
+    function isNotAlphanumeric(char) {
+        return /[^a-zA-Z0-9]/.test(char);
+    }
+
     function detectStdKeywords(theCode) {
 
         let wrap = (keywords, tag) => {
+
+            let ignoreOpenLenght = "<ignore>".length;
+            let ignoreCloseLenght = "</ignore>".length;
+
+            let ignore = false;
             
             keywords.forEach(keyword => {
 
                 for(i = 0; i <= theCode.length; i++) {
 
-                    if(theCode.substring(i, i + keyword.length) === keyword) {
+                    if(theCode.substring(i, i + ignoreOpenLenght) === "<ignore>") {
+                        ignore = true;
+                    }
+                    else 
+                    if(theCode.substring(i, i + ignoreCloseLenght) === "</ignore>") {
+                        ignore = false;
+                    }
 
-                        if(!isInsideStringOrComment(theCode, i, i + keyword.length)) {
-                            
-                            theCode = theCode.substring(0, i) + `<${tag}>${keyword}</${tag}>` + theCode.substring(i + keyword.length, theCode.length);
-                            
-                            i += `<${tag}>${keyword}</${tag}>`.length;
-                        }
+                    if(theCode.substring(i, i + keyword.length) === keyword && !ignore) {
+
+                        additionalKeywordChars.forEach(char => {
+
+                            if(theCode.substring(i, i + keyword.length + 1) === keyword + char) {
+                                
+                                let theCharBeforeTheKeyword = theCode.substring(i - 1, i);
+
+                                if(theCharBeforeTheKeyword !== "" && isNotAlphanumeric(theCharBeforeTheKeyword)) {
+
+                                    theCode = theCode.substring(0, i) + `<${tag}>${keyword}</${tag}>` + theCode.substring(i + keyword.length, theCode.length);
+                                    i += `<${tag}>${keyword}</${tag}>`.length + 1;                                    
+
+                                }
+                            }
+                        });
                     }
                 }
             });
@@ -261,6 +296,44 @@
 
         wrap(csharpStateSetOfKeywords, "stdKeyword");
         wrap(csharpBehaviorSetOfKeywords, "stdSpecKeyword");
+
+        return theCode;
+    }
+
+    function detectSeparateChars(theCode) {
+
+        let ignoreOpenLenght = "<ignore>".length;
+        let ignoreCloseLenght = "</ignore>".length;
+
+        let ignore = false;
+        let insideChar = false;
+
+        for(let i = 0; i <= theCode.length; i++) {
+            
+            if(theCode.substring(i, i + ignoreOpenLenght) === "<ignore>") {
+                ignore = true;
+            }
+            else 
+            if(theCode.substring(i, i + ignoreCloseLenght) === "</ignore>") {
+                ignore = false;
+            }
+
+            if(theCode[i] === "'" && !ignore && !insideChar) {
+                
+                insideChar = true;
+
+                theCode = theCode.slice(0, i) + "<ignore><chr>" + theCode.slice(i);
+                i += "<ignore><chr>".length;
+            }
+            else
+            if(theCode[i] === "'" && !ignore && insideChar) {
+                
+                insideChar = false;
+
+                theCode = theCode.slice(0, i + 1) + "</chr></ignore>" + theCode.slice(i + 1);
+                i += "</chr></ignore>".length;
+            }
+        }
 
         return theCode;
     }
@@ -284,7 +357,8 @@
         theCode = addColorSpanTags(theCode, "stdKeyword", "<span style=\"color: #0000FF\">");
         theCode = addColorSpanTags(theCode, "stdSpecKeyword", "<span style=\"color: #c204b2\">");
 
-        
+        theCode = detectSeparateChars(theCode);
+        theCode = addColorSpanTags(theCode, "chr", "<span style=\"color: #FF3131\">");
 
         //theCode = highlightCustomTypes(theCode); 
         //theCode = highlightNonStaticMethods(theCode); <methodName><span style="color: #7c3f00"> <span style="color: #0b856c">
@@ -292,9 +366,22 @@
         
         return `${divPreStart}${theCode}${divPreEnd}`;
     }
+    
+    /*
+    function showLoader() {
+        const loadingIndicator = document.getElementById("loadingSpinner");
+        loadingIndicator.style.display = "block";
+    }
+
+    function hideLoader() {
+        const loadingIndicator = document.getElementById("loadingSpinner");
+        loadingIndicator.style.display = "none";
+    }*/
 
     function convert() {
-        
+
+        //showLoader();
+
         var csharpSourceCodeTextarea = document.getElementById('csharp_source_code');
         var convertedCodeTextarea = document.getElementById('csharp_source_code_converted_into_html');
         var previewOutputDiv = document.getElementById('previewOutput');
@@ -318,4 +405,7 @@
         resultPreview.innerHTML = colorizedCode;
 
         previewOutputDiv.style.display = 'block';
+
+        //hideLoader()
+
     }
